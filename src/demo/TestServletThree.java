d9f63ec450dcf8e230ff0a2721e8689638bcaf7f
todayGitHub <351300531@qq.com>
2017-12-22 07:51:00 +0800

    save it

---
+++ src/demo/TestServletThree.java
@@ -0,0 +1,41 @@
+package demo;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestServletThree
+ */
+public class TestServletThree extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TestServletThree() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//request.getRequestDispatcher("/TestServletThree").forward(request, response);
+		System.out.println("three");
+		System.out.println(request.getAttribute("user"));
+	}
+
+}
